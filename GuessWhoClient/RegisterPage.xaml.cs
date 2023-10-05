9ee5e6a993cfd7baba2a75aff78c64cd5bcdd119
Angel Garcia <adjcg15@gmail.com>
2023-10-05 17:47:10 -0600

    feat(register page): logic for image upload handler

--- GuessWhoClient/RegisterPage.xaml.cs
+++ GuessWhoClient/RegisterPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace GuessWhoClient
 {
@@ -10,12 +13,21 @@ namespace GuessWhoClient
             InitializeComponent();
         }
 
-        private void BtnCreateAccountClick(object sender, RoutedEventArgs e)
+        private void BtnUploadProfilePictureClick(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Archivos de imagen|*.jpg;*.png;*.bmp|Todos los archivos|*.*";
 
+            if (fileDialog.ShowDialog() == true)
+            {
+                string imagePath = fileDialog.FileName;
+
+                BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath));
+                ImageProfile.Source = bitmapImage;
+            }
         }
 
-        private void BtnUploadProfilePictureClick(object sender, RoutedEventArgs e)
+        private void BtnCreateAccountClick(object sender, RoutedEventArgs e)
         {
 
         }
