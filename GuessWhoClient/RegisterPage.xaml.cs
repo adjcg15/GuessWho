3d0c52b910a51df37a25b4cb5d58d9e141b6543f
Angel Garcia <adjcg15@gmail.com>
2023-10-05 16:42:43 -0600

    feat(register page): register page UI implemented

--- GuessWhoClient/RegisterPage.xaml.cs
+++ GuessWhoClient/RegisterPage.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace GuessWhoClient
 {
@@ -19,6 +8,16 @@ namespace GuessWhoClient
         public RegisterPage()
         {
             InitializeComponent();
+        }
+
+        private void BtnCreateAccountClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnUploadProfilePictureClick(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
