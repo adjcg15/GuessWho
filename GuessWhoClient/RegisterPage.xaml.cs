78ee09bdabfca841d8b5d5edf5b187893f5edf45
Ángel de Jesús de la Cruz García <82688573+adjcg15@users.noreply.github.com>
2023-10-06 09:40:50 -0600

    merge(development): register
    * feat(register page): register page UI implemented
    
    * feat(register page): logic for image upload handler
    
    * feat(register page): client validations before make register in database
    
    * feat(register page): logic related to user service connection
    
    * feat(service connection): user service connection working

--- GuessWhoClient/RegisterPage.xaml.cs
+++ GuessWhoClient/RegisterPage.xaml.cs
@@ -1,24 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GuessWhoClient.Utils;
+using Microsoft.Win32;
+using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace GuessWhoClient
 {
     public partial class RegisterPage : Page
     {
+        private string imagePath = "";
+
         public RegisterPage()
         {
             InitializeComponent();
+        }
+
+        private void BtnUploadProfilePictureClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Archivos de imagen|*.jpg;*.png;";
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                imagePath = fileDialog.FileName;
+
+                BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath));
+                ImageProfile.Source = bitmapImage;
+            }
+        }
+
+        private void BtnCreateAccountClick(object sender, RoutedEventArgs e)
+        {
+            string fullname = TbName.Text;
+            string nickname = TbNickname.Text;
+            string password = TbPassword.Text;
+            string passwordConfirmation = TbRepeatedPassword.Text;
+            string email = TbEmail.Text;
+            byte[] profileImage = GetProfileImageBytes();
+
+            if (nickname == "" || password == "" || passwordConfirmation == ""
+               || email == "")
+            {
+                MessageBox.Show(
+                    "ingrese información en cada uno de los campos para poder continuar",
+                    "campos vacíos",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+                return;
+            }
+
+            if (!Authentication.IsValidEmail(email))
+            {
+                MessageBox.Show(
+                    "corrija el correo electrónico a un formato válido",
+                    "correo electrónico inválido",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+                return;
+            }
+
+            if (password != passwordConfirmation)
+            {
+                MessageBox.Show(
+                    "las contraseñas ingresadas no coinciden, modifique los campos para que lo hagan",
+                    "contraseñas no coincidentes",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+                return;
+            }
+
+            GameServices.UserServiceClient userServiceClient = new GameServices.UserServiceClient();
+            GameServices.Profile profile = new GameServices.Profile
+            {
+                Email = email,
+                FullName = fullname,
+                Password = Authentication.HashPassword(password),
+                NickName = nickname,
+                Avatar = profileImage
+            };
+
+            bool result = userServiceClient.RegisterUser(profile);
+            if (result)
+            {
+                MessageBox.Show("Se ha registrado el usuario correctamente");
+            }
+        }
+
+        private byte[] GetProfileImageBytes()
+        {
+            if (imagePath == "")
+            {
+                return null;
+            }
+
+            return File.ReadAllBytes(imagePath);
         }
     }
 }
