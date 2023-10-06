38f572baad1e45c9e7b0eff99aa060def821a243
Angel Garcia <adjcg15@gmail.com>
2023-10-06 16:37:47 -0600

    feat(client pages): confirm account page interface

---
+++ GuessWhoClient/ConfirmAccountPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+using System.Net.Mail;
+using System.Net;
+
+namespace GuessWhoClient
+{
+    public partial class ConfirmAccountPage : Page
+    {
+        private string nickname;
+        private string email;
+        private string password;
+        private string fullName;
+        private byte[] profileImage;
+
+        public ConfirmAccountPage()
+        {
+            InitializeComponent();
+        }
+
+        public ConfirmAccountPage(string nickname, string email, string password, string fullName, byte[] profileImage)
+        {
+            InitializeComponent();
+
+            this.nickname = nickname;
+            this.email = email;
+            this.password = password;
+            this.fullName = fullName;
+            this.profileImage = profileImage;
+        }
+
+        private void PageLoaded(object sender, RoutedEventArgs e)
+        {
+            string confirmationCode = GenerateConfirmationCode(10);
+            bool confirmationSent = SendConfirmationEmail(email, confirmationCode);
+
+            if(!confirmationSent)
+            {
+                MessageBox.Show(
+                    "Ocurrió un error al enviar el correo electrónico, intente registrarse en otro momento",
+                    "Error de envío",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+            }
+        }
+
+        private void BtnConfirmAccountClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private static string GenerateConfirmationCode(int length)
+        {
+            const string charactersSet = "abcdefghijklmnopqrstuvwxyz0123456789";
+            StringBuilder confirmationCode = new StringBuilder(length);
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int characterPosition = random.Next(0, charactersSet.Length);
+                confirmationCode.Append(charactersSet[characterPosition]);
+            }
+
+            return confirmationCode.ToString();
+        }
+
+        private static bool SendConfirmationEmail(string email, string code)
+        {
+            bool successSend = true;
+
+            try
+            {
+                SmtpClient smtpClient = new SmtpClient("localhost");
+                smtpClient.Port = 25;
+
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress("noreply@guesswho.com");
+                mailMessage.To.Add(new MailAddress(email));
+                mailMessage.Subject = "Confirmación de correo electrónico";
+                mailMessage.Body = "Tu código de confirmación: " + code;
+
+                smtpClient.Send(mailMessage);
+            } 
+            catch(SmtpException ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                successSend = false;
+            }
+
+            return successSend;
+        }
+    }
+}
--- GuessWhoClient/RegisterPage.xaml.cs
+++ GuessWhoClient/RegisterPage.xaml.cs
@@ -33,85 +33,86 @@ namespace GuessWhoClient
 
         private void BtnCreateAccountClick(object sender, RoutedEventArgs e)
         {
-            string fullname = TbName.Text;
+            string fullName = TbName.Text;
             string nickname = TbNickname.Text;
             string password = TbPassword.Text;
             string passwordConfirmation = TbRepeatedPassword.Text;
             string email = TbEmail.Text;
             byte[] profileImage = GetProfileImageBytes();
 
-            if (nickname == "" || password == "" || passwordConfirmation == ""
-               || email == "")
-            {
-                MessageBox.Show(
-                    "Ingrese información en cada uno de los campos para poder continuar",
-                    "Campos vacíos",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Warning
-                );
-                return;
-            }
+            //if (nickname == "" || password == "" || passwordConfirmation == ""
+            //   || email == "" || fullName == "")
+            //{
+            //    MessageBox.Show(
+            //        "Ingrese información en cada uno de los campos para poder continuar",
+            //        "Campos vacíos",
+            //        MessageBoxButton.OK,
+            //        MessageBoxImage.Warning
+            //    );
+            //    return;
+            //}
 
-            if (!Authentication.IsValidEmail(email))
-            {
-                MessageBox.Show(
-                    "Corrija el correo electrónico a un formato válido",
-                    "Correo electrónico inválido",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Warning
-                );
-                return;
-            }
+            //if (!Authentication.IsValidEmail(email))
+            //{
+            //    MessageBox.Show(
+            //        "Corrija el correo electrónico a un formato válido",
+            //        "Correo electrónico inválido",
+            //        MessageBoxButton.OK,
+            //        MessageBoxImage.Warning
+            //    );
+            //    return;
+            //}
 
-            if (Authentication.IsSecurePassword(password))
-            {
-                MessageBox.Show(
-                    "La contraseña debe tener mínimo 8 caracteres y, al menos, una letra minúscula, una letra mayúscula, un número y un caracter especial (¡, @, -, $, #, %, _, etc.).",
-                    "Contraseñas insegura",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Warning
-                );
-                return;
-            }
+            //if (Authentication.IsSecurePassword(password))
+            //{
+            //    MessageBox.Show(
+            //        "La contraseña debe tener mínimo 8 caracteres y, al menos, una letra minúscula, una letra mayúscula, un número y un caracter especial (¡, @, -, $, #, %, _, etc.).",
+            //        "Contraseñas insegura",
+            //        MessageBoxButton.OK,
+            //        MessageBoxImage.Warning
+            //    );
+            //    return;
+            //}
 
-            if (password != passwordConfirmation)
-            {
-                MessageBox.Show(
-                    "Las contraseñas ingresadas no coinciden, modifique los campos para que lo hagan",
-                    "Contraseñas no coincidentes",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Warning
-                );
-                return;
-            }
+            //if (password != passwordConfirmation)
+            //{
+            //    MessageBox.Show(
+            //        "Las contraseñas ingresadas no coinciden, modifique los campos para que lo hagan",
+            //        "Contraseñas no coincidentes",
+            //        MessageBoxButton.OK,
+            //        MessageBoxImage.Warning
+            //    );
+            //    return;
+            //}
 
-            GameServices.UserServiceClient userServiceClient = new GameServices.UserServiceClient();
-            bool userEmailAlreadyRegistered = userServiceClient.VerifyUserRegisteredByEmail(email);
-            if (userEmailAlreadyRegistered)
-            {
-                MessageBox.Show(
-                    "Ya existe una cuenta registrada con ese correo electrónico, intente con otro",
-                    "Correo electrónico registrado",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Warning
-                );
-                return;
-            }
+            //GameServices.UserServiceClient userServiceClient = new GameServices.UserServiceClient();
+            //bool userEmailAlreadyRegistered = userServiceClient.VerifyUserRegisteredByEmail(email);
+            //if (userEmailAlreadyRegistered)
+            //{
+            //    MessageBox.Show(
+            //        "Ya existe una cuenta registrada con ese correo electrónico, intente con otro",
+            //        "Correo electrónico registrado",
+            //        MessageBoxButton.OK,
+            //        MessageBoxImage.Warning
+            //    );
+            //    return;
+            //}
 
-            MessageBox.Show(nickname);
-            bool userNicknameAlreadyRegistered = userServiceClient.VerifyUserRegisteredByNickName(nickname);
-            if (userNicknameAlreadyRegistered)
-            {
-                MessageBox.Show(
-                    "Ya existe otro jugador utilizando el nombre de usuario ingresado, intente con otro",
-                    "Nombre de usuario registrado",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Warning
-                );
-                return;
-            }
+            //MessageBox.Show(nickname);
+            //bool userNicknameAlreadyRegistered = userServiceClient.VerifyUserRegisteredByNickName(nickname);
+            //if (userNicknameAlreadyRegistered)
+            //{
+            //    MessageBox.Show(
+            //        "Ya existe otro jugador utilizando el nombre de usuario ingresado, intente con otro",
+            //        "Nombre de usuario registrado",
+            //        MessageBoxButton.OK,
+            //        MessageBoxImage.Warning
+            //    );
+            //    return;
+            //}
 
-            MessageBox.Show("Sí te puedes registrar :)");
+            ConfirmAccountPage confirmAccountPage = new ConfirmAccountPage(nickname, email, password, fullName, profileImage);
+            this.NavigationService.Navigate(confirmAccountPage);
         }
 
         private byte[] GetProfileImageBytes()
