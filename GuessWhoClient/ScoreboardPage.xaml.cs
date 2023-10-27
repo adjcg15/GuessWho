0ae6a42111a78ca380bf66723e9291ac769a0a70
Angel Garcia <adjcg15@gmail.com>
2023-10-26 19:53:03 -0600

    feat(scoreboard view): scoreboard view with table structure and mock data

---
+++ GuessWhoClient/ScoreboardPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GuessWhoClient
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Score { get; set; }
+    }
+
+    public partial class Scoreboard : Page
+    {
+        public Scoreboard()
+        {
+            InitializeComponent();
+
+            ObservableCollection<Player> players = new ObservableCollection<Player>();
+            for(int i = 1; i < 16; i++)
+            {
+                players.Add(new Player { 
+                    Name = "Player " + i, 
+                    Position = (16 - i).ToString(), 
+                    Score=((16-i) * 100).ToString()
+                });
+            }
+            DataGridPlayers.ItemsSource = players;
+        }
+    }
+}
