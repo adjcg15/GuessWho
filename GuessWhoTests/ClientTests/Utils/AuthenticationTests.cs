11a9dca5be38e4574125f1991c12b6950071b230
Angel Garcia <adjcg15@gmail.com>
2023-11-22 23:30:28 -0600

    feat(GuessWhoTests): configuration of tests with Entity Framework

--- GuessWhoTests/ClientTests/Utils/AuthenticationTests.cs
+++ GuessWhoTests/ClientTests/Utils/AuthenticationTests.cs
@@ -1,9 +1,5 @@
 ﻿using GuessWhoClient.Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace GuessWhoTests.ClientTests.Utils
 {
---
+++ GuessWhoTests/DataAccessTests/UserDAOTests.cs
@@ -0,0 +1,59 @@
+﻿using GuessWhoDataAccess;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace GuessWhoTests.DataAccessTests
+{
+    public class UserDAOTests: IDisposable
+    {
+		private const string TEST_ACCOUNT_EMAIL = "unit-testing@test.com";
+		private const string TEST_ACCOUNT_PASSWORD_HASH = "b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79";
+
+		public UserDAOTests() 
+		{
+			using (var context = new GuessWhoContext())
+            {
+				Account testAccount = new Account
+				{
+					email = TEST_ACCOUNT_EMAIL,
+					password = TEST_ACCOUNT_PASSWORD_HASH
+				};
+				context.Accounts.Add(testAccount);
+                context.SaveChanges();
+			}
+		}
+
+		public void Dispose()
+		{
+			using (var context = new GuessWhoContext())
+			{
+				Account testAccount = context.Accounts.FirstOrDefault(a => a.email == TEST_ACCOUNT_EMAIL);
+				context.Accounts.Remove(testAccount);
+				context.SaveChanges();
+			}
+		}
+
+		[Fact]
+        public void TestVerifyUserSessionSuccess()
+        {
+            string email = TEST_ACCOUNT_EMAIL;
+            string hashedPassword = TEST_ACCOUNT_PASSWORD_HASH;
+
+            var account = UserDAO.VerifyUserSession(email, hashedPassword);
+            
+            Assert.NotNull(account);
+		}
+
+		[Fact]
+		public void TestVerifyUserSessionFail()
+		{
+			string email = "not-registered-email@gmail.com";
+			string hashedPassword = "b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79";
+
+			var account = UserDAO.VerifyUserSession(email, hashedPassword);
+
+			Assert.Null(account);
+		}
+    }
+}
