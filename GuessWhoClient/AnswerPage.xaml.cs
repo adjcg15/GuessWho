7653a038f40114d18725f6a19f201db477d37a7a
engaherr <kikgamboa@gmail.com>
2023-11-26 22:11:36 -0600

    feat(client): answer page GUI added, check and cross icons updated to a larger size

---
+++ GuessWhoClient/AnswerPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GuessWhoClient
+{
+    /// <summary>
+    /// Lógica de interacción para AnswerPage.xaml
+    /// </summary>
+    public partial class AnswerPage : Page
+    {
+        public AnswerPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnReportPlayerClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswerNoClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswerYesClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
