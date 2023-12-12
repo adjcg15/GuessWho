9a6600e11d382680bae8d442299087ee4de254e5
Angel Garcia <adjcg15@gmail.com>
2023-12-12 15:55:01 -0600

    feat(ScoreboardPage): communication exceptions handling and Player class moved to its own file

---
+++ GuessWhoClient/Model/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace GuessWhoClient
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Score { get; set; }
+    }
+}
--- GuessWhoClient/ScoreboardPage.xaml.cs
+++ GuessWhoClient/ScoreboardPage.xaml.cs
@@ -1,30 +1,20 @@
-﻿using GuessWhoClient.Utils;
-using System;
-using System.Collections.Generic;
+﻿using GuessWhoClient.GameServices;
+using GuessWhoClient.Utils;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace GuessWhoClient
 {
-    public class Player
-    {
-        public string Name { get; set; }
-        public string Position { get; set; }
-        public string Score { get; set; }
-    }
-
     public partial class ScoreboardPage : Page
     {
-        private ObservableCollection<GameServices.TopPlayer> topPlayers { get; set; }
+        private ObservableCollection<TopPlayer> topPlayers { get; set; }
         
         public ScoreboardPage()
         {
             InitializeComponent();
-            LoadBestPlayers("", 100);
         }
 
         private void BtnSearchPlayersClick(object sender, RoutedEventArgs e)
@@ -33,35 +23,60 @@ namespace GuessWhoClient
             LoadBestPlayers(query, 10);
         }
 
+        private void PageLoaded(object sender, RoutedEventArgs e)
+        {
+            LoadBestPlayers("", 100);
+        }
+
         private void LoadBestPlayers(string query, int numberOfPlayers)
         {
-            var userServiceClient = new GameServices.PlayerServiceClient();
-            var response = userServiceClient.GetBestPlayers(query, numberOfPlayers);
-
-            if (response.StatusCode == GameServices.ResponseStatus.OK)
+            try
             {
-                if(response.Value.Length > 0)
+                var userServiceClient = new PlayerServiceClient();
+                var response = userServiceClient.GetBestPlayers(query, numberOfPlayers);
+
+                if (response.StatusCode == ResponseStatus.OK)
                 {
-                    topPlayers = new ObservableCollection<GameServices.TopPlayer>(response.Value.ToList());
-                    DataGridPlayers.ItemsSource = topPlayers;
-                    ShowScoreboard();
+                    if (response.Value.Length > 0)
+                    {
+                        topPlayers = new ObservableCollection<GameServices.TopPlayer>(response.Value.ToList());
+                        DataGridPlayers.ItemsSource = topPlayers;
+                        ShowScoreboard();
+                    }
+                    else
+                    {
+                        ShowEmptyScoreboardMessage();
+                    }
                 }
                 else
                 {
                     ShowEmptyScoreboardMessage();
+                    MessageBox.Show(
+                        ServerResponse.GetMessageFromStatusCode(response.StatusCode),
+                        ServerResponse.GetTitleFromStatusCode(response.StatusCode),
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                    );
                 }
-                
             }
-            else
+            catch (EndpointNotFoundException ex)
             {
-                ShowEmptyScoreboardMessage();
-                MessageBox.Show(
-                    ServerResponse.GetMessageFromStatusCode(response.StatusCode),
-                    ServerResponse.GetTitleFromStatusCode(response.StatusCode),
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Warning
-                );
+                ServerResponse.ShowServerDownMessage();
+                App.log.Fatal(ex.Message);
+                RedirectToMainMenu();
             }
+            catch (CommunicationException ex)
+            {
+                ServerResponse.ShowConnectionLostMessage();
+                App.log.Error(ex.Message);
+                RedirectToMainMenu();
+            }
+        }
+
+        private void RedirectToMainMenu()
+        {
+            MainMenuPage mainMenu = new MainMenuPage();
+            NavigationService.Navigate(mainMenu);
         }
 
         private void ShowEmptyScoreboardMessage()
