f3621fbb1bb00c0f58678a7650a38bcce7445d4a
engaherr <kikgamboa@gmail.com>
2023-12-13 00:27:26 -0600

    feat(tests): user dao test added

--- GuessWhoTests/DataAccessTests/UserDAOTests.cs
+++ GuessWhoTests/DataAccessTests/UserDAOTests.cs
@@ -1,6 +1,9 @@
 ﻿using GuessWhoDataAccess;
+using Moq;
 using System;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Linq.Expressions;
 using Xunit;
 
 namespace GuessWhoTests.DataAccessTests
@@ -9,6 +12,9 @@ namespace GuessWhoTests.DataAccessTests
     {
 		private const string TEST_ACCOUNT_EMAIL = "unit-testing@test.com";
 		private const string TEST_ACCOUNT_PASSWORD_HASH = "b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79";
+        private const string TEST_USER_NICKNAME = "TESTING";
+        private const string TEST_USER_FULLNAME = "TEST";
+        private Account globalTestAccount = new Account();
 
 		public UserDAOTests() 
 		{
@@ -21,6 +27,8 @@ namespace GuessWhoTests.DataAccessTests
 				};
 				context.Accounts.Add(testAccount);
                 context.SaveChanges();
+
+                globalTestAccount = testAccount;
 			}
 		}
 
@@ -31,7 +39,7 @@ namespace GuessWhoTests.DataAccessTests
 				Account testAccount = context.Accounts.FirstOrDefault(a => a.email == TEST_ACCOUNT_EMAIL);
 				context.Accounts.Remove(testAccount);
 				context.SaveChanges();
-			}
+            }
 		}
 
 		[Fact]
@@ -53,7 +61,76 @@ namespace GuessWhoTests.DataAccessTests
 
 			var account = UserDAO.VerifyUserSession(email, hashedPassword);
 
-			Assert.Null(account);
+			Assert.Null(account.Value);
 		}
+
+        [Fact]
+        public void TestRegisterUserSuccess()
+        {
+            User testUser = new User
+            {
+                fullName = TEST_USER_FULLNAME,
+                nickname = TEST_USER_NICKNAME
+            };
+
+            Account testAccount = new Account
+            {
+                email = TEST_ACCOUNT_EMAIL,
+                password = TEST_ACCOUNT_PASSWORD_HASH
+            };
+
+            var response = UserDAO.RegisterUser(testUser, testAccount);
+
+            Assert.Equal(ResponseStatus.OK, response.StatusCode);
+            Assert.True(response.Value);
+
+            using (var context = new GuessWhoContext())
+            {
+                User userToRemove = context.Users.FirstOrDefault(u => u.idAccount == testAccount.idAccount);
+                context.Users.Remove(userToRemove);
+
+                Account accountToRemove = context.Accounts.FirstOrDefault(a => a.idAccount == testAccount.idAccount);
+                context.Accounts.Remove(accountToRemove);
+
+                context.SaveChanges();
+            }
+        }
+
+        //[Fact]
+        //public void TestRegisterUserFail()
+        //{
+        //    var response = UserDAO.RegisterUser(null, null);
+
+        //    Assert.False(response.Value);
+        //    Assert.NotEqual(ResponseStatus.OK, response.StatusCode);
+        //}
+
+        [Fact]
+        public void TestGetUserByIdAccountSuccess()
+        {
+            var response = UserDAO.GetUserByIdAccount(globalTestAccount.idAccount);
+
+            Assert.Equal(ResponseStatus.OK, response.StatusCode);
+            Assert.NotNull(response.Value);
+        }
+
+        [Fact]
+        public void TestGetUserByIdAccountFail()
+        {
+            const int INVALID_ID_ACCOUNT = -1; 
+
+            var response = UserDAO.GetUserByIdAccount(INVALID_ID_ACCOUNT);
+
+            Assert.Null(response.Value);
+            Assert.Equal(ResponseStatus.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public void TestGetUserByEmailSuccess()
+        {
+            var response = UserDAO.GetUserByEmail(TEST_ACCOUNT_EMAIL);
+
+            Assert.NotNull(response.Value);
+        }
     }
 }
