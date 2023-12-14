067e08b01f76bb77b00c72c76eb9d218c348f199
engaherr <kikgamboa@gmail.com>
2023-12-14 01:25:54 -0600

    fix(tests services): GuessWhoServices removed from tests project cause already had gameServices from client project

---
+++ GuessWhoTests/ServicesTests/GameService/MockGameServiceClient.cs
@@ -0,0 +1,17 @@
+﻿using GuessWhoClient.GameServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessWhoTests.ServicesTests.GameService
+{
+    public class MockGameServiceClient : IGameServiceCallback
+    {
+        public void PlayerStatusInMatchChanged(PlayerInMatch player, bool isInMatch)
+        {
+
+        }
+    }
+}
